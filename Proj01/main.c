168ee1d653c7c180ded2f5bc8250355320e4bfb1
William-Fonseca-Geralde <127157689+William-Fonseca-Geralde@users.noreply.github.com>
2024-02-28 15:22:37 -0300

    pequenas mudanças, mas ainda sem resultado

--- Proj01/main.c
+++ Proj01/main.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
   printf("Olá, Mundo\n");
 
-  char nome[30], sexo[2];
+  char nome[30], sexo;
   int idade;
   float peso, tam;
 
   
   printf("Digite o seu nome: ");
   scanf("%s", &nome);
+  printf("Digite seu gênero: ");
+  scanf(" %*c", &sexo);
   printf("Digite sua idade: ");
   scanf("%i", &idade);
   printf("Digite seu peso: ");
   scanf("%f", &peso);
-  printf("Digite seu gênero: ");
-  scanf("%s", &sexo);
   printf("Digite seu Tamanho: ");
   scanf("%f", &tam);
 
-  printf("\nNome: %s \nIdade: %i \nPeso: %.2f \nGênero: %s \nTamanho: %.2f", nome, idade, peso, sexo, tam);
+  printf("\nNome: %s \nIdade: %i \nPeso: %.2f \nGênero: %c \nTamanho: %.2f", nome, idade, peso, sexo, tam);
   
   return 0;
 }
