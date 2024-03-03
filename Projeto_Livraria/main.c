2fb0c82886c79f14d01ce3a32492721621984d10
William-Fonseca-Geralde <127157689+William-Fonseca-Geralde@users.noreply.github.com>
2024-03-03 12:35:54 -0300

    criação das structs

--- Projeto_Livraria/main.c
+++ Projeto_Livraria/main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct livro {
+  char nome_livro, data_criacao[10], autor;
+  int num_pag;
+} Livro;
+
+typedef struct no {
+  Livro livro;
+  No *prox;
+} *No;
+
+No *inicio = NULL;
+
 int main() {
   
   return 0;
